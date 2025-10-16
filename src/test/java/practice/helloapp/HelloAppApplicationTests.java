c026e7b2892d3484bf7e86525963a711b5277919
Suresh Karuppannan <kasuresh@athenahealth.com>
2025-10-16 22:33:36 +0530

    first commit

---
+++ src/test/java/practice/helloapp/HelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.helloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
